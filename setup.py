f0f68c15b2d88a0e185fcc8f07cc629ff845c84c
Cristian Măgherușan-Stanciu <cristi.magherusan@gmail.com>
2014-01-17 23:54:53 +0100

    packaging fixes
    - deploy files in the right place
    - created AUTHORS file and set myself as packager
    - initial test of DEB and RPM packaging
    - added to .gitignore the files generated when building DEB and RPM packages

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from distutils.core import setup
 setup(
     name='ec2-collective',
     version='0.0.1',
-    author='Anders Dyekjaer Hansen',
-    author_email='anders@dyejaer.dk',
+    author='Cristian Magherusan-Stanciu',
+    author_email='cristian.magherusan-stanciu@here.com',
     packages=[],
     scripts=['bin/ec2-cagent', 'bin/ec2-cmaster','bin/sqs-price-calculator'],
+    data_files=[('/etc/init.d', ['etc/init.d/ec2-cagent']),
+        ('/etc/ec2_collective', ['etc/ec2_collective/ec2-cagent.json', 'etc/ec2_collective/ec2-cmaster.json']),
+        ('/etc/logrotate.d', ['etc/logrotate.d/ec2-cagent'])
+      ],
     url='http://pypi.python.org/pypi/ec2-collective/',
     license='LICENSE.txt',
     description='Mcollective alternative using AWS SQS',
